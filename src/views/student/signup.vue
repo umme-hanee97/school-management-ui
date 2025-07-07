05d2fe95c5f8d20418381cace16c5ed18b050239
UmmeHanee1416 <73064213+UmmeHanee1416@users.noreply.github.com>
2025-07-07 21:32:25 +0600

    update: signup page

--- src/views/student/signup.vue
+++ src/views/student/signup.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="item-center w-1/3 m-auto mt-10 shadow-2xl rounded-lg p-10">
     <div class="w-5/6 m-auto space-y-4">
-      <fwb-Input
-        label="Name"
-        input-class="bg-slate-50 border border-sky-200 rounded-md text-black"
-        placeholder="Enter Your Name"
-      />
-      <fwb-Input
-        label="Email"
-        input-class="bg-slate-50 border border-sky-200 rounded-md text-black"
-        placeholder="Enter Your Email"
-      />
+      <fwb-Input label="Name" input-class="input-style" placeholder="Enter Your Name" />
+      <fwb-Input label="Email" input-class="input-style" placeholder="Enter Your Email" />
       <div>
         <label class="text-sm font-semibold">Class</label><br />
-        <select
-          class="focus:ring-sky-200 w-full bg-slate-50 border border-sky-300 rounded-md"
-        >
+        <select class="select-style">
           <option selected disabled>Select One</option>
           <option v-for="(value, key) in classes" :key="key">
             {{ value.name }}
@@ -24,31 +14,46 @@
       </div>
       <div>
         <label class="text-sm font-semibold">Section</label><br />
-        <select
-          class="focus:ring-sky-200 w-full bg-slate-50 border border-sky-300 rounded-md"
-        >
+        <select class="select-style">
           <option selected disabled>Select One</option>
           <option v-for="(value, key) in sections" :key="key">
             {{ value.name }}
           </option>
         </select>
       </div>
-      <fwb-Input
-        type="number"
-        label="Roll No."
-        input-class="bg-slate-50 border border-sky-200 rounded-md text-black"
-        placeholder="Enter Your Roll No"
-      />
+      <fwb-Input type="number" label="Roll No." input-class="input-style" placeholder="Enter Your Roll No" />
       <div>
         <label class="text-sm font-semibold">Photo</label>
-        <fwb-file-input dropzone input-class="bg-slate-50 border border-sky-200 rounded-md text-black"/>
+        <div class="flex items-center justify-center w-full">
+          <label for="dropzone-file"
+            class="flex flex-col items-center justify-center w-full h-64 border border-sky-200 rounded-md cursor-pointer bg-slate-50">
+            <div class="flex flex-col items-center justify-center pt-5 pb-6" id="dropzone">
+              <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+              </svg>
+              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span class="font-semibold">Click to upload</span> or drag and drop
+              </p>
+              <p class="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <div class="flex flex-col items-center justify-center w-full h-screen">
+              <img id="image-preview" class="hidden w-32 h-32 object-cover rounded-md" />
+            </div>
+            <input id="dropzone-file" type="file" class="hidden" @change="showImage" />
+          </label>
+        </div>
       </div>
+      <fwb-button gradient="cyan-blue" class="w-full">Create Account</fwb-button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { FwbSelect, FwbInput, FwbFileInput } from "flowbite-vue";
+import { FwbSelect, FwbInput, FwbFileInput, FwbButton } from "flowbite-vue";
 
 const classes = [
   { value: "1", name: "One" },
@@ -60,7 +65,22 @@ const sections = [
   { value: "2", name: "Grape" },
   { value: "3", name: "Watermelon" },
 ];
+
+function showImage() {
+  const fileInput = document.getElementById("dropzone-file");
+  const imagePreview = document.getElementById("image-preview");
+  const svgDiv = document.getElementById("dropzone");
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      svgDiv.classList.add("hidden");
+      imagePreview.classList.remove("hidden");
+      imagePreview.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+}
 </script>
 
-<style>
-</style>
+<style></style>
